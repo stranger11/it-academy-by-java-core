bb039d78b930727c5efb374f74aae44e22ae56b1
Yan Pashyk <71889627+stranger11@users.noreply.github.com>
2020-09-30 23:23:32 +0300

    Delete Hello.java

--- Hello.java
+++
@@ -1,7 +0,0 @@
-public class Hello{
-    public static void mail(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello Belarus!");
-        System.out.println("I like Java!");
-    }
-}
