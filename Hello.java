bb37f74b2dd6ac3caaa09a2d7793ffc753d31cdd
Yan Pashyk <71889627+stranger11@users.noreply.github.com>
2020-10-01 21:08:56 +0300

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,5 @@
 public class Hello{
     public static void main (String[] args) {
-        System.out.println("Hello World!");
         System.out.println("Hello Belarus!");
         System.out.println("I like Java!");
     }
